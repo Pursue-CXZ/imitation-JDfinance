0de6fb4fa1db52d7bbe50f00b82a7bfc21b079a0
Pursue-CXZ <2622009279@qq.com>
2019-05-28 00:11:53 +0800

    完成京东金融首页组件内容

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -2,16 +2,38 @@
     <div>
         <all-header></all-header>
         <hslider></hslider>
+        <Novice></Novice>
+        <Borrow></Borrow>
+        <Money></Money>
+        <Product></Product>
+        <Life></Life>
+        <Footer></Footer>
+        <Navbar></Navbar>
     </div>
 </template>
 
 <script>
 import allHeader from '../public/header.vue'
 import hslider from './hslider.vue'
+import Novice from './novice.vue'
+import Borrow from './borrow.vue'
+import Money from './money.vue'
+import Product from './product.vue'
+import Life from './life.vue'
+import Footer from '../public/footer.vue'
+import Navbar from '../public/navbar.vue'
+
 export default {
     components:{
         allHeader,
-        hslider
+        hslider,
+        Novice,
+        Borrow,
+        Money,
+        Product,
+        Life,
+        Footer,
+        Navbar
     }
 }
 </script>
