5eaf6329a3a8070c0b276f9364851d65faba919e
Pursue-CXZ <2622009279@qq.com>
2019-05-28 22:24:19 +0800

    完成京东金融白条的页面制作

---
+++ app/js/ious/index.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+    <div>
+        <all-header></all-header>
+        <Islider></Islider>
+        <Welfare></Welfare>
+        <Service></Service>
+        <Footer></Footer>
+        <Navbar></Navbar>
+    </div>
+</template>
+
+<script>
+    import allHeader from '../public/header.vue'
+    import Islider from './islider.vue'
+    import Welfare from './welfare.vue'
+    import Service from './service.vue'
+    import Footer from '../public/footer.vue'
+    import Navbar from '../public/navbar.vue'
+
+    export default {
+        components:{
+            allHeader,
+            Islider,
+            Welfare,
+            Service,
+            Footer,
+            Navbar
+        }
+    }
+</script>
+
