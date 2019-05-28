065bffcde932b74810cea04c262a42be73b645e9
Pursue-CXZ <2622009279@qq.com>
2019-05-29 00:31:08 +0800

    完成京东金融众筹页面的制作

---
+++ app/js/raise/index.vue
@@ -0,0 +1,43 @@
+<template lang="html">
+    <div>
+        <all-header></all-header>
+        <Rslider></Rslider>
+        <Daily></Daily>
+        <In></In>
+        <More></More>
+        <Footer cname="mfooter"></Footer>
+        <Navbar></Navbar>
+    </div>
+</template>
+
+<script>
+    import allHeader from '../public/header.vue'
+    import Rslider from './rslider.vue'
+    import Daily from './daily.vue'
+    import In from './in.vue'
+    import More from './more.vue'
+    import Footer from '../public/footer.vue'
+    import Navbar from '../public/navbar.vue'
+
+    export default {
+        components:{
+            allHeader,
+            Rslider,
+            Daily,
+            In,
+            More,
+            Footer,
+            Navbar
+        }
+    }
+</script>
+<style lang="scss">
+    .mfooter{
+        margin-top: 0 !important;
+        ul{
+            li:first-child{
+                display: none;
+            }
+        }
+    }
+</style>
