572efcbd20b1895ae4b83719df440bf77755252a
Pursue-CXZ <48152698+Pursue-CXZ@users.noreply.github.com>
2019-07-10 23:24:17 +0800

    Update main.js

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,7 @@ import router from "./router"
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+
 new Vue({
     el: "#app",
     router,
@@ -13,4 +13,3 @@ new Vue({
        return c(App)
     }
 })
-/* eslint-enable no-new */
